45822bd94e4ab3fd95c818a58d6e6302269ffa5c
John Daniel L. Ubungen <jdubungen805@gmail.com>
2025-10-02 13:09:36 +0800

    Create ArraySample_3_Ubungen.java

---
+++ Midterm Codes/Module 2 -  List and Array/M_Activity3_Ubungen/ArraySample_3_Ubungen.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.arraysample_3;
+
+import java.io.IOException;
+
+/**
+ *
+ * @John Daniel Ubungen,BCS23
+ */
+public class ArraySample_3 {
+    
+     public ArraySample_3() {
+         // Declare and create an array size of 100
+         int[] arr;
+    arr = new int [100];
+    int nItems=10; // number of elements
+    int i;// loop counter 
+    
+    // Elements and there assigned values 
+    arr[0] = 5;
+    arr[1] = 10;
+    arr[2] = 15;
+    arr[3] = 50;
+    arr[4] = 56;
+    arr[5] = 62;
+    arr[6] = 77;
+    arr[7] = 81;
+    arr[8] = 95;
+    arr[9] = 101;
+    
+    // Display the elements of the array
+    System.out.print("Elements of the array: ");
+    for(i=0; i<nItems; i++)
+        System.out.print(arr[i] + " ");
+    }
+
+    public static void main(String[] args) throws IOException {
+        // An Object to run the constructor
+        ArraySample_3 samp = new ArraySample_3();    
+    }
+}
